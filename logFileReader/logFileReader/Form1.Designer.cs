ae5d658d843077627e269c28f60e000c330e0909
kevin <kevinbolan@bellsouth.net>
2016-06-09 08:16:53 -0400

    no message

---
+++ logFileReader/logFileReader/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace logFileReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.numberSent = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.successTextBox = new System.Windows.Forms.RichTextBox();
+            this.failedTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numberSuccess = new System.Windows.Forms.Label();
+            this.numberFailed = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numberSent
+            // 
+            this.numberSent.AutoSize = true;
+            this.numberSent.Location = new System.Drawing.Point(341, 26);
+            this.numberSent.Name = "numberSent";
+            this.numberSent.Size = new System.Drawing.Size(0, 13);
+            this.numberSent.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(32, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(32, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(82, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Total Number of Charts Sent";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(82, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(180, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Total Number of Successful Uploads";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(82, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Total Number of Failed Uploads";
+            // 
+            // successTextBox
+            // 
+            this.successTextBox.Location = new System.Drawing.Point(3, 0);
+            this.successTextBox.Name = "successTextBox";
+            this.successTextBox.Size = new System.Drawing.Size(453, 218);
+            this.successTextBox.TabIndex = 6;
+            this.successTextBox.Text = "";
+            // 
+            // failedTextBox
+            // 
+            this.failedTextBox.Location = new System.Drawing.Point(0, 3);
+            this.failedTextBox.Name = "failedTextBox";
+            this.failedTextBox.Size = new System.Drawing.Size(456, 215);
+            this.failedTextBox.TabIndex = 7;
+            this.failedTextBox.Text = "";
+            this.failedTextBox.TextChanged += new System.EventHandler(this.failedTextBox_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(127, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Browse Files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numberSuccess
+            // 
+            this.numberSuccess.AutoSize = true;
+            this.numberSuccess.Location = new System.Drawing.Point(341, 76);
+            this.numberSuccess.Name = "numberSuccess";
+            this.numberSuccess.Size = new System.Drawing.Size(0, 13);
+            this.numberSuccess.TabIndex = 11;
+            // 
+            // numberFailed
+            // 
+            this.numberFailed.AutoSize = true;
+            this.numberFailed.Location = new System.Drawing.Point(341, 126);
+            this.numberFailed.Name = "numberFailed";
+            this.numberFailed.Size = new System.Drawing.Size(0, 13);
+            this.numberFailed.TabIndex = 12;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 151);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(467, 247);
+            this.tabControl1.TabIndex = 13;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.successTextBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(459, 221);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Successfully Uploaded Files";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.failedTextBox);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(459, 221);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Failed Uploaded Files";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(127, 453);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(211, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Export to Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.chart1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(459, 221);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Number";
+            legend1.Title = "Number";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(3, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Number";
+            series1.Name = "Number";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(453, 218);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 495);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.numberFailed);
+            this.Controls.Add(this.numberSuccess);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numberSent);
+            this.Name = "Form1";
+            this.Text = "Log File Transmittal Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numberSent;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RichTextBox successTextBox;
+        private System.Windows.Forms.RichTextBox failedTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label numberSuccess;
+        private System.Windows.Forms.Label numberFailed;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ logFileReader/logFileReader/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace logFileReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader file = new System.IO.StreamReader(openFileDialog1.FileName);
+                int totalSentCounter = 0;
+                int totalSuccessCounter = 0;
+                int totalFailCounter = 0;
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+
+                    if (line.Contains("File,"))
+                    {
+                        totalSentCounter++;
+
+                        if (file.ReadLine().Contains("ERROR"))
+                        {
+                            String[] errorFile = line.Split(',');
+                            failedTextBox.AppendText(errorFile[3] + "\n");
+                            totalFailCounter++;
+                        }
+                        else
+                        {
+                            String[] successFile = line.Split(',');
+                            successTextBox.AppendText(successFile[3] + "\n");
+                            totalSuccessCounter++;
+                        }
+                    }
+
+                }
+
+                file.Close();
+                numberSent.Text = totalSentCounter.ToString();
+                numberSuccess.Text = totalSuccessCounter.ToString();
+                numberFailed.Text = totalFailCounter.ToString();
+                button2.Visible = true;
+                chart1.Series["Number"].Points.Add(totalSuccessCounter).Name="Success";
+                chart1.Series["Number"].Points.Add(totalFailCounter);
+
+            }
+        }
+
+        private void failedTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createPieChart(int success, int failed)
+        {
+            //Chart pieChart = new Chart();
+            
+        }
+    }
+
+}
